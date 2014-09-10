20a31c227a4f4d4987866f74e9c3bab44061d480
Logan Serman <logan.serman@metova.com>
2014-09-10 16:55:53 -0500

    Fix for breakage when the routes file has constraints (they will not be recognized by recognize_path)

--- lib/avocado/middleware/resource_serialization.rb
+++ lib/avocado/middleware/resource_serialization.rb
@@ -2,7 +2,7 @@ module Avocado
   class Middleware::ResourceSerialization
 
     def call(example, request, response)
-      name = infer_name_from_route(request.path, request.method) || ""
+      name = infer_name_from_route(request.path, request.method) || infer_name_from_controller(request.params['controller']) || ''
       Avocado::Cache.json.merge! resource: { name: name }
       yield
     end
@@ -11,11 +11,14 @@ module Avocado
 
       def infer_name_from_route(path, method)
         controller = Rails.application.routes.recognize_path(path, method: method)[:controller]
+        infer_name_from_controller(controller)
+      rescue ActionController::RoutingError
+        nil
+      end
+
+      def infer_name_from_controller(controller)
         name = controller.partition('/').reject(&:blank?).last
         name.titleize.split('/').last
-      rescue ActionController::RoutingError
-        puts "Path #{path} not found in controller routes; Defaulting name to path."
-        path
       end
 
   end
